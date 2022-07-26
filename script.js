fec725b404bfe18df24dd0c362b5797f95b301bf
Bea Reid <beareid.info@gmail.com>
2022-07-25 23:11:05 -0500

    Update display numbers
    - Add operator to the end of the top display operand
    - Create function that displays commas in the display numbers

--- script.js
+++ script.js
@@ -67,9 +67,34 @@ class Calculator {
     this.prevOperand = '';
   }
 
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]);
+    const decimalDigits = stringNumber.split('.')[1];
+    let integerDisplay;
+
+    if (isNaN(integerDigits)) {
+      integerDisplay = '';
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+    }
+
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    }
+    else {
+      return integerDisplay;
+    }
+  }
+ 
   updateDisplay() {
-    this.currOperandTextEle.innerText = this.currOperand;
-    this.prevOperandTextEle.innerText = this.prevOperand;
+    this.currOperandTextEle.innerText = this.getDisplayNumber(this.currOperand);
+    if(this.operation != null) {
+      this.prevOperandTextEle.innerText = `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`;
+    }
+    else {
+      this.prevOperandTextEle.innerText = '';
+    }
   }
 }
 
